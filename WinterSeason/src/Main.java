3f6891f25ec68b153d182eee18f14531723bcb1b
athillen <athillen@student.ethz.ch>
2020-04-01 21:29:31 +0200

    Winter Season

--- WinterSeason/src/Main.java
+++ WinterSeason/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class Main {
     public static void main(String[] args) {
         // Uncomment this line if you want to read from a file
@@ -20,7 +22,7 @@ class Main {
         for (int i = 0; i < n; i++) {
             probs[i] = In.readDouble();
         }
-        Out.println(probabilityUtil(probs, 0, k));
+        Out.println(probability(probs, k));
     }
 
     public static double probabilityUtil(double[] probs, int index, int nP) {
@@ -41,6 +43,35 @@ class Main {
             double p2 = (1 - probs[index-1])*probabilityUtil(probs, index, nP);
 //            System.out.println(index + " p1 " + p1+ " p2 " + p2 + "probs index: " + probs[index-1]);
             return p1 + p2;
+        }
+    }
+
+    public static double probability(double[] probs, int nP) {
+        double [][] DP = new double[probs.length][probs.length+1];
+        DP[0][0] = (1-probs[0]);
+        DP[0][1] = probs[0];
+        for (int i = 1; i < probs.length; i++) {
+            DP[i][0] = (1-probs[i]) * DP[i-1][0];
+        }
+
+        for (int i = 1; i < probs.length; i++) {
+                for (int j = 1; j < probs.length + 1; j++) {
+                    DP[i][j] = DP[i - 1][j] * (1 - probs[i]) + DP[i - 1][j - 1] * probs[i];
+                }
+
+        }
+        double sum = 0;
+        for (int i = nP; i < probs.length+1; i++) {
+            sum += DP[probs.length-1][i];
+        }
+//        System.out.println();
+//        printArray(DP);
+        return sum;
+    }
+
+    public static void printArray(double[][] arr) {
+        for (double[] i: arr) {
+            System.out.println(Arrays.toString(i));
         }
     }
 }
