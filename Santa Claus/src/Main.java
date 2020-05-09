c534ff0c1346bf7f60def59db59c0d1b86a5ea3e
athillen <athillen@student.ethz.ch>
2020-05-09 15:09:47 +0200

    Finish Santa Claus

--- Santa Claus/src/Main.java
+++ Santa Claus/src/Main.java
@@ -24,35 +24,42 @@ class Main {
         int m  = In.readInt(); // nr. toys
         int d[] = new int[n]; // deserves
         int c[] = new int[m]; // count toys
-
+        int sum = 0; // sum of deserved toys
         Algorithms.Graph G = new Algorithms.Graph(n + m + 2);
 
         for (int j = 0; j < n; j++) { // j child index
             d[j] = In.readInt();
+            sum += d[j];
         }
 
-        for (int i = 0; i < n; i++) { // i toy index
+        for (int i = 0; i < m; i++) { // i toy index
             c[i] = In.readInt();
         }
 
-        for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             // connecting children to T
-
+            G.addEdge(2 + m + j, 1, d[j]);
         }
 
-        for (int j = 0; j < m; j++) {
+        for (int i = 0; i < m; i++) {
+            // Connecting Santa to toys
+            G.addEdge(0, 2 + i, c[i]);
         }
 
         for (int j = 0; j < n; j++) {
             for (int i = 0; i < m; i++) {
-                G.addEdge();
+//                connecting toys and children
+                G.addEdge(2 + i, 2+m+j, 1);
             }
+        }
+
+        if (G.computeMaximumFlow(0, 1) >= sum) {
+            Out.println("yes");
+        } else {
+            Out.println("no");
         }
 
 
 
-
-        // Output using Out.java class
-        Out.println(n + n);
     }
 }
